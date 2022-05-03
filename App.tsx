2f0f26dd9d32bce7f2d3b9c64a71e755ad7c93c2
Lucas Dantas <lucasdantasmoura01@gmail.com>
2022-05-03 20:31:52 -0300

    feat(styling): Adding Styled Components, responsive fontsize, linear gradient. Add barcode-scanner lib

--- App.tsx
+++ App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
 import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
 import { Routes } from './src/routes';
 import store from './src/store';
+import theme from './src/theme';
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <StatusBar backgroundColor="transparent" translucent />
-      <Routes />
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <StatusBar backgroundColor="transparent" translucent />
+        <Routes />
+      </Provider>
+    </ThemeProvider>
     )
 }
 
