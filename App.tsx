f224034ee82ff5458faf41fba5bc9bd47cdc7bf9
Lucas Dantas <lucasdantasmoura01@gmail.com>
2022-05-03 22:01:11 -0300

    feat(library): Add gesture handler

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
+import { StatusBar, StyleSheet } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
 import { Routes } from './src/routes';
@@ -8,12 +9,19 @@ import theme from './src/theme';
 
 export default function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Provider store={store}>
-        <StatusBar backgroundColor="transparent" translucent />
-        <Routes />
-      </Provider>
-    </ThemeProvider>
+     <GestureHandlerRootView style={styles.container}>
+      <ThemeProvider theme={theme}>
+        <Provider store={store}>
+          <StatusBar backgroundColor="transparent" translucent />
+          <Routes />
+        </Provider>
+      </ThemeProvider>
+     </GestureHandlerRootView>
     )
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
